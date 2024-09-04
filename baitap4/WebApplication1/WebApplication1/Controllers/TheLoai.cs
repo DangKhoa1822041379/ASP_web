a45dc15d795e389e245275849b264166b7e5afe2
Đăng Khoa <1822041379@dntu.edu.vn>
2024-09-04 18:14:32 +0700

    Action result

---
+++ baitap4/WebApplication1/WebApplication1/Controllers/TheLoai.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TheLoai : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Ok("Id: " + id);
+        }
+
+        //public IActionResult Detail(int id,string ten)
+        //{
+        //    return Content("Id: " + id +" ; ten: "+ ten);
+        //}
+
+
+        public IActionResult Detail(int id,string ten)
+        {
+            //    return Content("Id: " + id +" ; ten: "+ ten);
+            return Content(string.Format("id: {0}; ten: {1}",id, ten));
+        }
+
+        public IActionResult Show(List<string> categories)
+        {
+            string content = "Danh sách thể loại: ";
+            foreach (var category in categories)
+            {
+                content = content + " " + category + ", ";
+            }
+            return Content(content);
+        }
+    }
+}
