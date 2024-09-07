f1e44e671ef01da76f22489cb9192d2b93b5e5ac
Đăng Khoa <1822041379@dntu.edu.vn>
2024-09-07 09:45:50 +0700

    Da xong BTVN

---
+++ BTVN/BTVN/Controllers/BTVNController.cs
@@ -0,0 +1,56 @@
+﻿using BTVN.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTVN.Controllers
+{
+    public class BTVNController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Hoten = "Nguyễn HOàng Đăng Khoa";
+            ViewBag.MSSV = "1822041379";
+            ViewData["Nam"] = "2004";
+            return View();
+        }
+
+        public IActionResult MayTinh(int a, int b, string pheptinh)
+        {
+            int result = 0;
+
+            switch (pheptinh)
+            {
+                case "cong":
+                    result = a + b;
+                    break;
+                case "tru":
+                    result = a - b;
+                    break;
+                case "nhan":
+                    result = a * b;
+                    break;
+                case "chia":
+                    if (b != 0)
+                    {
+                        result = a / b;
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "Không thể chia cho 0.";
+                    }
+                    break;
+                default:
+                    ViewBag.ErrorMessage = "Phép tính không hợp lệ.";
+                    break;
+            }
+
+            ViewBag.Result = result;
+
+            return View("MayTinh");
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+
+}
