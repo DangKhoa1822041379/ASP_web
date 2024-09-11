890c04dc4731ee07d43b36736fbc124d97b8883e
Đăng Khoa <1822041379@dntu.edu.vn>
2024-09-11 16:33:39 +0700

    update

---
+++ baitap6/baitap6/Controllers/TaiKhoanController.cs
@@ -0,0 +1,21 @@
+﻿using baitap6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baitap6.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taiKhoan)
+        {
+            if (taiKhoan == null && taiKhoan.Password != null && taiKhoan.Password.Length > 0)
+            {
+                taiKhoan.Password = taiKhoan.Password + "_da_ma_hoa";
+            }
+            return View(taiKhoan);
+        }
+    }
+}
