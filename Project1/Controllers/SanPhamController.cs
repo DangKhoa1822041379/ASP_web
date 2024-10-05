ea79512c25803882c2946b4264212eb68835a00f
Đăng Khoa <1822041379@dntu.edu.vn>
2024-10-06 00:54:44 +0700

    update

--- Project1/Controllers/SanPhamController.cs
+++ Project1/Controllers/SanPhamController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Project1.Data;
@@ -7,6 +9,7 @@ using Project1.Models;
 namespace Project1.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class SanPhamController : Controller
     {
         private readonly ApplicationDbContext _db;
--- Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ Project1/obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebfa64bb5e7aac1afe0b11327076b82c33b4e55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4722dda041021269b8faeaa13464b1c5b7b3254")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
